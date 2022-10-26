060f077151c7f15b5cd41e1747353ce1d7934563
afrialivan <afrialivan0@gmail.com>
2022-10-27 01:28:23 +0800

    lanjut besok

--- beranda.js
+++ beranda.js
@@ -1,26 +1,35 @@
+var wwindow = window.innerWidth;
+if(wwindow < 1000){
+  document.location.href='catering.html'
+}
+
 window.addEventListener('scroll', (event) => {
-  const logo = document.getElementById('tes')
+  const logoContainer = document.getElementById('tes')
+  const logo = document.getElementById('logo')
+  const judul = document.getElementById('judul')
+  const subJudul = document.getElementById('sub-judul')
+  
   var wwindow = window.innerWidth;
   let scroll = this.scrollY;
-  var lebar = wwindow / 2 - logo.clientWidth / 2;
+  var lebar = wwindow / 2 - logoContainer.clientWidth / 2;
+  // logoContainer.style.position='fixed'
+  logoContainer.style.left=lebar + 'px';
   // let posisix1 = 1000;
   // let posisix = 1000 + scroll;
   console.log(scroll);
-  logo.style.left=lebar + 'px';
-  if(scroll > 85){
-    logo.style.transition='1s';
-    logo.style.position='relative'
-    // logo.classList.add('bg')
-    // logo.classList.add('display')
+  if(scroll < 85){
+    // logoContainer.style.transition='1s';
+    // logoContainer.style.top='600px'
+    // logoContainer.style.backgroundColor='black'
+    // logoContainer.classList.remove('sticky')
+    // // logo.style.width='400px';
+    // judul.style.fontSize='150px';
+    // subJudul.style.fontSize='70px';
+    logoContainer.style.textAlign='center';
+    // logoContainer.style.top='200px';
   }
-  else if(scroll < 85){
-    logo.style.top='200px';
-    logo.style.position='sticky'
-    // logo.style.width='100px';
-    // logo.style.height='100px';
-    // logo.style.top='100px';
-    // logo.style.left='10px'
-    logo.style.transition='1s';
-    // logo.classList.remove('bg')
+  else if(scroll > 500){
+    logoContainer.style.left='4000px'
+    document.location.href='catering.html'
   }
 })
